53cade6fdccd320d9ce8cd5dc73bd2ccad86a54a
geekerlw <geekerlw@gmail.com>
2017-06-12 09:35:42 +0800

    modify compile error when use the newest compiler

--- drivers/net/wireless/rockchip_wlan/rtl8192eu/core/rtw_security.c
+++ drivers/net/wireless/rockchip_wlan/rtl8192eu/core/rtw_security.c
@@ -1538,7 +1538,9 @@ _func_enter_;
 
     /* Insert MIC into payload */
     for (j = 0; j < 8; j++)
+	{
     	pframe[payload_index+j] = mic[j];	//message[payload_index+j] = mic[j];
+	}
 
 	payload_index = hdrlen + 8;
 	for (i=0; i< num_blocks; i++)
@@ -1923,7 +1925,9 @@ _func_enter_;
 
     /* Insert MIC into payload */
     for (j = 0; j < 8; j++)
+	{
     	message[payload_index+j] = mic[j];
+	}
 
 	payload_index = hdrlen + 8;
 	for (i=0; i< num_blocks; i++)
